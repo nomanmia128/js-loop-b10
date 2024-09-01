170bafafe50f49006f225a9b3e0cc9755f639912
Noman <nomanmia128@gmail.com>
2024-09-02 00:42:21 +0600

    added js loop

---
+++ for-of.js
@@ -0,0 +1,19 @@
+const numbers = [12, 34, 54, 67,87,98];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
+
+
+const fruits = ['Apple', 'Mango', 'Oreng', 'Whatermslon', 'jackfruits'];
+
+// for(){
+
+// }
+
+for(const frut of fruits){
+    console.log(frut);
+    console.log('I went to eat')
+}
---
+++ while.js
@@ -0,0 +1,17 @@
+const num = 70;
+if(num < 10 ){
+    console.log('cheto akta numer pico');
+}
+console.log('samting cic no raber');
+
+
+
+// ------- WHILE --- Loop----------
+let num1 = 0;
+
+while( num1 < 5){
+    console.log('loooooooooooop', num1);
+    num1 = num1 + 1;
+    // num1 += 1;
+    // num1++;
+}
